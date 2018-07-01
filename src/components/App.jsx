f2386e0a17f05f6db87289dec67c6456e343ae33
CARLOS AGUIRRE <carlos.agu@gmail.com>
2018-07-01 13:50:19 -0600

    Inicio del Proyecto

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className='display-4'>My React App</h1>
+                <p>Ejemplo una app de react desde cero</p>
+            </div>
+        )
+    }
+}
